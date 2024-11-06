24aa1ba7ff2343906a51c2247ba22676982e43a2
bitotamas <bito.tamas51@gmail.com>
2024-11-06 13:30:36 +0100

    Foundation
    Üres, futtatható, létrehozott projekt

---
+++ notebook_webalk/src/main/java/notebook_webalk/NotebookWebappMain.java
@@ -0,0 +1,13 @@
+package notebook_webalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookWebappMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookWebappMain.class, args);
+	}
+
+}
---
+++ notebook_webalk/src/test/java/notebook_webalk/NotebookWebappMainTests.java
@@ -0,0 +1,13 @@
+package notebook_webalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookWebappMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
